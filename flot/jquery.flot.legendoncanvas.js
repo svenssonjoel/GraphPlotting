338cac7a2aa3e0b96dcee44892a85a84b22a7359
Joel Svensson <svenssonjoel@yahoo.se>
2014-08-07 16:09:12 -0400

    update

--- flot/jquery.flot.legendoncanvas.js
+++ flot/jquery.flot.legendoncanvas.js
@@ -14,11 +14,14 @@ var options = plot.getOptions();
 if(!options.legend.show) return;
 
 var placeholder = plot.getPlaceholder();
+// var container = options.legend.container; || placeholder.find('.legend');
 var container = options.legend.container || placeholder.find('.legend');
+
+
 
 var f = {
 style: placeholder.css("font-style"),
-size: Math.round(0.8 * (+placeholder.css("font-size").replace("px", "") || 13)),
+size: 24, //Math.round(0.8 * (+placeholder.css("font-size").replace("px", "") || 13)),
 variant: placeholder.css("font-variant"),
 weight: placeholder.css("font-weight"),
 family: placeholder.css("font-family")
@@ -31,6 +34,7 @@ ctx.textBaseline = "bottom";
 function fontAscent() {
 return 12;
 }
+
 
 var series = plot.getData();
 var plotOffset = plot.getPlotOffset();
@@ -50,11 +54,12 @@ if(lf) label = lf(label, s);
 labelWidth = ctx.measureText(label).width;
 if(labelWidth > legendWidth) legendWidth = labelWidth
 }
-var LEGEND_BOX_WIDTH = 22; // color box
+var LEGEND_BOX_WIDTH = 45; //22; // color box
 var PADDING_RIGHT = 5;
-var LEGEND_BOX_LINE_HEIGHT = 18;
+var LEGEND_BOX_LINE_HEIGHT = 32; //18;
+var PADDING_TOP = 5;
 legendWidth = legendWidth + LEGEND_BOX_WIDTH + PADDING_RIGHT;
-legendHeight = num_labels * LEGEND_BOX_LINE_HEIGHT;
+legendHeight = num_labels * (LEGEND_BOX_LINE_HEIGHT + PADDING_TOP);
 var x, y;
 if(options.legend.container != null) {
 x = $(options.legend.container).offset().left;
@@ -84,23 +89,30 @@ ctx.globalAlpha = 1.0;
 }
 }
 var posx, posy;
+var B_WIDTH = 32; 
+var B_HEIGHT = 24;
+var FONT_PAD_LEFT = 38;
+var B_PAD_TOP  = 5;
 for(var i = 0; i < series.length; ++i) {
 s = series[i];
 label = s.label;
 if(!label) continue;
 if(lf) label = lf(label, s);
-posy = y + (i * 18);
+posy = y + (i * (24 + B_PAD_TOP));
 ctx.fillStyle = options.legend.labelBoxBorderColor;
-ctx.fillRect(x, posy, 18, 14);
+ctx.fillRect(x, posy, B_WIDTH, B_HEIGHT); //18, 14);
 ctx.fillStyle = "#FFF";
-ctx.fillRect(x + 1, posy + 1, 16, 12);
+ctx.fillRect(x + 1, posy + 1, B_WIDTH-2, B_HEIGHT-2); //16, 12);
 ctx.fillStyle = s.color;
-ctx.fillRect(x + 2, posy + 2, 14, 10);
-posx = x + 22;
-posy = posy + f.size + 2;
+ctx.fillRect(x + 2, posy + 2, B_WIDTH-4,B_HEIGHT-4); //14, 10);
+//posx = x + 22;
+//posy = posy + f.size + 2;
+posx = x + FONT_PAD_LEFT;
+posy = posy + B_HEIGHT + B_PAD_TOP; 
 
-ctx.fillStyle = options.grid.color;
-ctx.fillText(label, posx, posy);
+//ctx.fillStyle = options.grid.color;
+ctx.fillStyle = options.legend.color || options.grid.color;
+ctx.fillText(label, posx, (posy - i));
 }
 
 container.hide(); // hide the HTML version
